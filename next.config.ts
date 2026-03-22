a6625905e9d2f695769ee10ce78f56f2a6e49430
tiago <tiago@grupoeureka.com.br>
2026-03-22 14:10:05 -0300

    te4

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   env: {
     NEXT_PUBLIC_GEMINI_API_KEY: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
-    NEXT_PUBLIC_PUSHER_KEY: process.env.NEXT_PUBLIC_PUSHER_KEY,
-    NEXT_PUBLIC_PUSHER_CLUSTER: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
+    NEXT_PUBLIC_PUSHER_KEY: 'cbefe2fef0f08ab319f4',
+    NEXT_PUBLIC_PUSHER_CLUSTER: 'sa1',
     PUSHER_APP_ID: '2131067',
     PUSHER_SECRET: '3ded18905f7c8c76de96',
   },
